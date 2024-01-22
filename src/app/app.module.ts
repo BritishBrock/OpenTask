844033cb13989cae67b7fb83ed1a6089db709ca6
DESKTOP-TJAHBVG\Brock <brockaban@gmail.com>
2024-01-22 18:38:14 +0100

    Creating component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskViewerComponent } from './task-viewer/task-viewer.component';
 import { TaskComponent } from './components/task/task.component';
+import { TasklistComponent } from './components/tasklist/tasklist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskViewerComponent,
-    TaskComponent
+    TaskComponent,
+    TasklistComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/tasklist/tasklist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tasklist',
+  templateUrl: './tasklist.component.html',
+  styleUrl: './tasklist.component.scss'
+})
+export class TasklistComponent {
+
+}
