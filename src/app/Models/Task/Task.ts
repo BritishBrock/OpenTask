bd6bb0e766acbe0de7c3e6d9f69e91c76b5f6f9a
DESKTOP-TJAHBVG\Brock <brockaban@gmail.com>
2024-01-24 20:17:56 +0100

    Initial styling for task complete.

--- src/app/Models/Task/Task.ts
+++ src/app/Models/Task/Task.ts
@@ -1,8 +1,13 @@
 export class Task{
     id:number;
     name:string;
-    constructor(id:number,name:string){
+    colorTag:string;
+    constructor(id:number,name:string,colorTag:string){
         this.id = id;
         this.name= name;
+        this.colorTag = colorTag;
+    }
+    changeColorTag(){
+
     }
 }
