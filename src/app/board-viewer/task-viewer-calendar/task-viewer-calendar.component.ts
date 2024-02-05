62f87f81b93c44b11e120041e80dbb23c4029442
DESKTOP-TJAHBVG\Brock <brockaban@gmail.com>
2024-02-05 22:47:32 +0100

    Added calender and ways for the task to be inserted in the end date.

--- src/app/board-viewer/task-viewer-calendar/task-viewer-calendar.component.ts
+++ src/app/board-viewer/task-viewer-calendar/task-viewer-calendar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Task } from '../../Models/Task/Task';
+import { TaskViewerBoardService } from '../../Services/taskViewerBoard/task-viewer-board.service';
 
 @Component({
   selector: 'app-task-viewer-calendar',
@@ -7,16 +9,74 @@ import { Component } from '@angular/core';
 })
 export class TaskViewerCalendarComponent {
 
+
+  constructor(private TaskViewerBoardService:TaskViewerBoardService){}
+
   currentYear:any;
   selectYear:any;
   selectMonth:any;
   monthAndYear:any;
   months:any;
   currentMonth:any;
+  today:any;
+
+  taskEndDates:any = {};
+
+  addToDateMap(task:Task){
+    if(Object.hasOwn(this.taskEndDates,task.endDate+"")){
+      this.taskEndDates[task.endDate+""].push(task)
+    }else{
+      this.taskEndDates[task.endDate+""] = [];
+      this.taskEndDates[task.endDate+""].push(task)
+    }
+  }
+
 
   ngOnInit(){
     
-    
+ 
+
+    for(let i = 0;i <this.TaskViewerBoardService.globalTasks.length;i++){
+       if(this.TaskViewerBoardService.globalTasks[i].endDate) this.addToDateMap(this.TaskViewerBoardService.globalTasks[i])
+    }  
+    for(let i = 0;i < this.TaskViewerBoardService.globalTaskLists.length;i++){
+      for(let y = 0; y < this.TaskViewerBoardService.globalTaskLists[i].tasks.length;y++){
+        if(this.TaskViewerBoardService.globalTaskLists[i].tasks[y].endDate) this.addToDateMap(this.TaskViewerBoardService.globalTaskLists[i].tasks[y])
+      }
+    }  
+
+  
+
+
+
+    this.today = new Date();
+    this.currentMonth = this.today.getMonth();
+    this.currentYear = this.today.getFullYear();
+ 
+    this.months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+   
+ 
+   this.showCalendar(this.currentMonth, this.currentYear);
+
+
+   this.addTasksToCalender();
+
+
+
+  }
+
+
+  addTasksToCalender(){
+    Object.entries(this.taskEndDates).every(([key,values])=>{
+
+      if(this.currentMonth == new Date(key).getMonth() && this.currentYear == new Date(key).getFullYear()){
+        let value:any = values;
+        if(value.length == 1){
+          document.getElementById("C-"+new Date(key).getDate())!.textContent += value[0].name;
+        }
+      }
+      return true;
+    })
   }
 
    next() {
@@ -44,8 +104,6 @@ export class TaskViewerCalendarComponent {
     let daysInMonth = 32 - new Date(year, month, 32).getDate();
 
     let tbl = document.getElementById("calendar-body"); // body of the calendar
-    console.log(tbl)
-    console.log("f")
     // clearing all previous cells
     tbl!.innerHTML = "";
 
@@ -56,12 +114,14 @@ export class TaskViewerCalendarComponent {
     for (let i = 0; i < 6; i++) {
         // creates a table row
         let row = document.createElement("tr");
+        row.classList.add("calRow")
 
         //creating individual cells, filing them up with data.
         for (let j = 0; j < 7; j++) {
             if (i === 0 && j < firstDay) {
                 let cell = document.createElement("td");
                 let cellText = document.createTextNode("");
+                cell.classList.add("tableCell")
                 cell.appendChild(cellText);
                 row.appendChild(cell);
             }
@@ -72,10 +132,15 @@ export class TaskViewerCalendarComponent {
             else {
               let cell = document.createElement("td");
               let cellText = document.createTextNode(date +"");
+              let c = date+"";
+              cell.onclick =()=>{this.cellCliked(c)}
               if (date === this.today.getDate() && year === this.today.getFullYear() && month === this.today.getMonth()) {
-                  cell.classList.add("bg-info");
-              } // color today's date
+                cell.classList.add("bg-info");
+            } // color today's date
+              cell.classList.add("tableCell")
+              cell.id = "C-"+c
               cell.appendChild(cellText);
+              
               row.appendChild(cell);
               date++;
             }
@@ -87,17 +152,19 @@ export class TaskViewerCalendarComponent {
     }
 
 }
-today:any;
-ngAfterViewInit(){
-   this.today = new Date();
-   this.currentMonth = this.today.getMonth();
-   this.currentYear = this.today.getFullYear();
 
-   this.months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-  
 
-  this.showCalendar(this.currentMonth, this.currentYear);
+
+cellCliked(date?:any){
+  console.log(""+date +" "+ this.currentMonth +" "+this.currentYear)
 }
- 
+
+
+
+
+
+
+
+
 }
 
