22561e1051b28cce204f746e9187344f1806eb2c
DESKTOP-TJAHBVG\Brock <brockaban@gmail.com>
2024-02-08 18:33:45 +0100

    Importing and saving boards to computer(safety not tested).

--- src/app/board-picker/board-picker.component.ts
+++ src/app/board-picker/board-picker.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Board } from '../Models/Board/Board';
 import { BoardService } from '../Services/board/board.service';
 import { Router } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-board-picker',
@@ -11,7 +12,8 @@ import { Router } from '@angular/router';
 export class BoardPickerComponent {
 
   boards:Board[] = [];
-  constructor(private boardService:BoardService,private Router:Router){}
+  file?:File;
+  constructor(private boardService:BoardService,private Router:Router,private sanitizer: DomSanitizer){}
 
 ngOnInit(){
   this.boards = this.boardService.globalBoards
@@ -23,4 +25,28 @@ ngOnInit(){
   addBoard(){
     this.boardService.addEmptyBoard();
   }
+
+checking:any;
+
+  check(event:any){
+    let file = event.target.files[0]
+    let fileReader = new FileReader();
+    fileReader.onload = (e:any) => {
+      this.checking = fileReader.result;
+      this.save()
+    }
+    fileReader.readAsText(file);
+
+  }
+  fileUrl:any
+  save(){
+
+
+    const data = this.checking;
+    const blob = new Blob([data], {
+      type: 'json'
+  });
+  this.fileUrl = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(blob));
+  }
+
 }
