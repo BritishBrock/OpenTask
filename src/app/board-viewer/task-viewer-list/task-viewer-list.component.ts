2319655c488906c8000361a1b8744b86edc0d522
DESKTOP-TJAHBVG\Brock <brockaban@gmail.com>
2024-02-21 18:04:58 +0100

    In list view add tasklist viewer, with tree for tasks in tasklist.

--- src/app/board-viewer/task-viewer-list/task-viewer-list.component.ts
+++ src/app/board-viewer/task-viewer-list/task-viewer-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TaskViewerBoardService } from '../../Services/taskViewerBoard/task-viewer-board.service';
 import { Task } from '../../Models/Task/Task';
+import { TaskList } from '../../Models/TaskList/TaskList';
 
 @Component({
   selector: 'app-task-viewer-list',
@@ -10,9 +11,12 @@ import { Task } from '../../Models/Task/Task';
 export class TaskViewerListComponent {
   
   constructor(private TaskViewerBoardService:TaskViewerBoardService){}
+  menuSelected:string = "task";
   tasks:Task[]= []
+  taskLists:TaskList[]= []
   ngOnInit(){
     this.tasks = [...this.TaskViewerBoardService.globalTasks];
+    this.taskLists = [...this.TaskViewerBoardService.globalTaskLists];
     for(let i = 0;i < this.TaskViewerBoardService.globalTaskLists.length;i++){
       for(let y = 0; y < this.TaskViewerBoardService.globalTaskLists[i].tasks.length;y++){
         this.tasks.push(this.TaskViewerBoardService.globalTaskLists[i].tasks[y])
