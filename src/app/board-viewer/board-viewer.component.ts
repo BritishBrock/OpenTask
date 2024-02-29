5ffc05d8467a7a41d9f6b88621a489a38336b77c
DESKTOP-TJAHBVG\Brock <brockaban@gmail.com>
2024-02-29 16:28:49 +0100

    Added js animation (will change to css) and made it a toggable feature in settings.

--- src/app/board-viewer/board-viewer.component.ts
+++ src/app/board-viewer/board-viewer.component.ts
@@ -9,6 +9,7 @@ import { BoardService } from '../Services/board/board.service';
 import { DBService } from '../Services/DB/db.service';
 import { BoardSerializer } from '../Models/Board/BoardSerializer';
 import { Board } from '../Models/Board/Board';
+import { SettingsService } from '../Services/settings/settings.service';
 
 @Component({
   selector: 'app-board-viewer',
@@ -24,13 +25,19 @@ export class BoardViewerComponent {
       private activeRoute:ActivatedRoute,
       private boardService:BoardService,
       private DBService:DBService,
+      private settings:SettingsService,
       ){}
 
       isQuickBoardSelectOpen:boolean = false;
       allBoards:Board[] = this.boardService.globalBoards;
       ngOnInit(){
+
+  
+
         let doesntExists = true;
+        
         this.activeRoute.params.subscribe((query)=>{
+          if(this.settings.userSettings.general.showLoadEffect)this.loadTransition();
           if( this.boardService.globalBoards.length == 0){
               this.DBService.openDB();
                 this.DBService.dbCompelte.subscribe((data) => {
@@ -64,6 +71,7 @@ export class BoardViewerComponent {
         this.router.navigateByUrl("/settings");
       }
       goToBoard(boardId:number){
+        this.isNavClosed = false
         this.router.navigateByUrl("/b/"+boardId);
       }
 
@@ -98,9 +106,62 @@ export class BoardViewerComponent {
       },
       
     ]
+    loadTransition(){
 
+      //want to change this to css animation.
 
+      this.done = false;
+      let html:HTMLElement =  this.elRef.nativeElement;
+      let transition = document.createElement("div");
+      let transition1 = document.createElement("div");
+      let transition2 = document.createElement("div");
+      transition.style.width = "100%";
+      transition.style.height = "100%";
+      transition.style.position = "fixed";
+      transition.style.left = "0px"
+      transition.style.top = "100%";
+      transition.style.backgroundColor = "black";
+      transition.style.zIndex = "2000000000";
 
+      transition1.style.width = "100%";
+      transition1.style.height = "100px";
+      transition1.style.position = "fixed";
+      transition1.style.left = "0px"
+      transition1.style.top = "100%";
+      transition1.style.backgroundColor = "#646464";
+      transition1.style.zIndex = "2000000000";
+
+      transition2.style.width = "100%";
+      transition2.style.height = "100px";
+      transition2.style.position = "fixed";
+      transition2.style.left = "0px"
+      transition2.style.top = "100%";
+      transition2.style.zIndex = "2000000000";
+      transition2.style.backgroundColor = "#cdcdcd";
+      let i = 150;
+      let i1 = 145;
+      let i2 = 140;
+      let x = setInterval(()=>{
+        transition.style.top = i+"%";
+        transition1.style.top = i1+"%";
+        transition2.style.top = i2+"%";
+        i--;
+        i1--;
+        i2--;
+        if(i == 0){
+          this.done = true;
+
+        }
+        if(i == -110){
+         clearInterval(x);
+        }
+      },5)
+
+      html.append(transition);
+      html.append(transition1);
+      html.append(transition2);
+    }
+    done:boolean = !this.settings.userSettings.general.showLoadEffect;
     returnToBoardSelction(){
       this.router.navigateByUrl("/");
     }
