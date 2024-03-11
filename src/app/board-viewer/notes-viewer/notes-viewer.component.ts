ade853e9e26589a014e0031c0d316cdf92f20482
DESKTOP-TJAHBVG\Brock <brockaban@gmail.com>
2024-03-11 20:56:29 +0100

    Simplified notes tab.

--- src/app/board-viewer/notes-viewer/notes-viewer.component.ts
+++ src/app/board-viewer/notes-viewer/notes-viewer.component.ts
@@ -18,24 +18,10 @@ export class NotesViewerComponent {
   ngOnInit() {
     this.notes = this.globalBoards.activeBoard!.boardNotes
   }
-  globalPos: any;
-  dropped  = false;
-  pos: any;
-  x: any;
-  y: any;
-  ofPos:any
+  
   noteTitle:string = "";
   noteDesc:string = "";
-  pickUpNote(note: any, pos: any) {
-    if(this.isNoteCreationActive){
-      this.createNote()
-    }else{
-      this.dropped = false;
-      this.ofPos = pos;
-      this.noteSelectd = this.notes.splice(pos, 1)[0];
-    }
-  }
-
+ 
   createNote(){
     if(!this.isNoteCreationActive)return;
     let n = new Note(this.noteTitle)
@@ -46,69 +32,8 @@ export class NotesViewerComponent {
     this.noteTitle = "";
     this.isNoteCreationActive = false;
   }
-  ngAfterContentChecked(): void {
-    this.changeDetector.detectChanges();
-  }
   isNoteCreationActive:boolean = false;
-
-  isOnOtherNote(x: any, y: any) {
-    let ns = document.getElementById('selected');
-    if (!ns) return;
-    let list: any = document.getElementsByClassName('list');
-
-    for (let i = 0; i < list.length; i++) {
-      if (
-        x > list[i].offsetLeft - 25 &&
-        x + ns.clientWidth < list[i].offsetLeft + list[i].clientWidth + 25 &&
-        y > list[i].offsetTop &&
-        y + 10 < list[i].offsetTop + list[i].clientHeight
-      )
-        return i;
-    }
-
-    return null;
+  deleteNote(index:any){
+    this.notes.splice(index,1);
   }
-
-  getGridSpan(id:number){
-      return "span " + Math.ceil(document.getElementById(id+"")!.getElementsByTagName("pre")[0].clientHeight / 200)
-  }
-
-
-  ngAfterViewInit(){
-    this.htmlElement =this.notebody.nativeElement;
-
-
-
-    this.htmlElement.addEventListener('mouseup', () => {
-      delete this.pos;
-      delete this.globalPos;
-      if (this.noteSelectd) delete this.noteSelectd;
-    });
-    this.htmlElement.addEventListener('mousemove', (e: any) => {
-      let ns = document.getElementById('selected');
-      if (!ns) return;
-      ns.style.position = 'absolute';
-      ns.style.left = e.x + 'px';
-      ns.style.top = e.y + 'px';
-      this.pos = this.isOnOtherNote(e.x, e.y);
-      if (this.pos != null) {
-        if(this.globalPos != null){
-       this.notes.splice(this.globalPos ,1); 
-       delete this.globalPos
-        }
-          this.notes.splice(this.pos,0,this.noteSelectd)
-          this.globalPos = this.pos;
-          s.next(true)
-          this.changeDetector.detectChanges();
-      }
-
-
-    });
-  }
-
-
 }
-
-
-
-export var s:Subject<boolean> = new Subject<boolean>()
