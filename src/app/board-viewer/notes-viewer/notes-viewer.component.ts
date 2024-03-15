bd953c5356ab5ec274d038756110cd2ef3f7a965
DESKTOP-TJAHBVG\Brock <brockaban@gmail.com>
2024-03-15 22:26:48 +0100

    Changed notes styling to show when note is selected to switch on phone view.

--- src/app/board-viewer/notes-viewer/notes-viewer.component.ts
+++ src/app/board-viewer/notes-viewer/notes-viewer.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectorRef, Component, ElementRef, ViewChild } from '@angular/core';
 import { Note } from '../../Models/Note/Note';
-import { Subject } from 'rxjs';
+import { Subject, first } from 'rxjs';
 import { BoardService } from '../../Services/board/board.service';
 import {
   style,
@@ -112,16 +112,36 @@ pos:any = {x:0,y:0}
       }
      
     })
+    
     this.htmlElement.addEventListener("mouseup",(event:any)=>{
       if(!this.noteGrabbed) return;
       delete this.noteGrabbed;
     })
-
-  }
+    this.htmlElement.addEventListener("touchstart",(event:any)=>{
+     
+   
+      if(this.isFirstTouch){this.count++;}
+      if(this.count > 1 ){
+        this.isFirstTouch = false;
+        this.count = 0;
+        if(this.noteSwitch && !this.isFirstTouch){
+          document.getElementById(this.noteSwitch.id)!.style.border = "none";
+         
+          delete this.notePos;
+       delete this.noteSwitch;
   
+        }
+      }
+
+     
+ 
+
+    })
+  }
+  count =0;
   noteTitle:string = "";
   noteDesc:string = "";
- 
+  isFirstTouch:any =true;
   createNote(){
     if(!this.isNoteCreationActive)return;
     let n = new Note(this.noteTitle)
@@ -147,28 +167,31 @@ pos:any = {x:0,y:0}
    document.getElementById("copy")!.style.left =event.x +"px";
    document.getElementById("copy")!.style.top =event.y +"px";
   }
-  switchStart(note:any,index:number){
-    if(!this.noteSwitch){
-      this.noteSwitch = note;
-      this.notePos = index;
-    }else{
-
-      
-
-
-
-        // this.notes.splice(index,1)
-        // this.notes.splice(this.notePos,1)
-
-        // this.notes.splice(index,0,this.noteSwitch)
-        // this.notes.splice(this.notePos,0,note)
-
-      this.notes[index]= this.noteSwitch;
-      this.notes[this.notePos]= note;
-
-     delete this.notePos;
-     delete this.noteSwitch;
-    }
+  switchStart(note:any,index:number,event:any){
+      event.preventDefault()
+      if(!this.noteSwitch){
+        document.getElementById(note.id)!.style.border = "5px solid blue";
+        this.noteSwitch = note;
+        this.notePos = index;
+        this.isFirstTouch = true;
+      }else{
+  
+        
+  
+  
+  
+          // this.notes.splice(index,1)
+          // this.notes.splice(this.notePos,1)
+  
+          // this.notes.splice(index,0,this.noteSwitch)
+          // this.notes.splice(this.notePos,0,note)
+          this.notes[index]= this.noteSwitch;
+          this.notes[this.notePos]= note;
+          document.getElementById(this.noteSwitch.id)!.style.border = "none";
+       delete this.notePos;
+       delete this.noteSwitch;
+      }
+    
   }
   notePos:any;
   noteSwitch:any;
